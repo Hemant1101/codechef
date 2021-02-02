4162127e0e090820d1b9cb73c38e8e4efe450e92
Hemant Kumar <63533985+Hemant1101@users.noreply.github.com>
2021-02-02 15:23:58 +0530

    Update divthree.c

--- divthree.c
+++ divthree.c
@@ -1,3 +1,8 @@
+// author:Hemant1101
+// lang: C
+// topic: codechef problem ( file name is the problem code )
+
+//without using function
 #include <stdio.h>
 
 int main()
@@ -31,44 +36,49 @@ int main()
     }
     return 0;
 }
-// #include <stdio.h>
 
-// int maxday(int sum, int n, int k, int d)
-// {
-//     int day = 0;
-//     if (sum >= (d * k))
-//     {
-//         return d;
-//     }
-//     else
-//     {
-//         while ((d * k) > sum)
-//         {
-//             d--;
-//             day = d;
-//         }
-//     }
-//     return day;
-// }
+// using function
 
-// int main()
-// {
-//     int t;
-//     scanf("%d", &t);
-//     while (t--)
-//     {
-//         int n, k, d;
-//         int day;
-//         scanf("%d %d %d", &n, &k, &d);
-//         int sum = 0;
-//         int a;
-//         for (int i = 0; i < n; i++)
-//         {
-//             scanf("%d", &a);
-//             sum = sum + a;
-//         }
-//         day = maxday(sum, n, k, d);
-//         printf("%d\n", day);
-//     }
-//     return 0;
-// }
+/*
+#include <stdio.h>
+
+int maxday(int sum, int n, int k, int d)
+{
+    int day = 0;
+    if (sum >= (d * k))
+    {
+        return d;
+    }
+    else
+    {
+        while ((d * k) > sum)
+        {
+            d--;
+            day = d;
+        }
+    }
+    return day;
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int n, k, d;
+        int day;
+        scanf("%d %d %d", &n, &k, &d);
+        int sum = 0;
+        int a;
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &a);
+            sum = sum + a;
+        }
+        day = maxday(sum, n, k, d);
+        printf("%d\n", day);
+    }
+    return 0;
+}
+*/
